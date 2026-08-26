28b5aa55d72efad58c0354e7f336b9a8f688d0d2
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-26 18:29:19 +0530

    Simple pattern

--- question.cpp
+++ question.cpp
@@ -50,3 +50,13 @@ int main(){
     cout<<"****\n***\n**\n*\n";
     return 0;
 }
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"****\n";
+    cout<<"***\n";
+    cout<<"**\n";
+    cout<<"*\n";
+    return 0;
+}
