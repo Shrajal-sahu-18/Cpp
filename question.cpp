9cbe0f9df27f5be5a608a6ef6e72e6eb6da83638
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-01 21:51:13 +0530

    positive negative zero

--- question.cpp
+++ question.cpp
@@ -121,3 +121,9 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
+    if (n > 0)
+        cout << "Positive";
+    else if (n < 0)
+        cout << "Negative";
+    else
+        cout << "Zero";
