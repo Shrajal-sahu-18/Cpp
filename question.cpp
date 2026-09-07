c3c31f4370aeded2f9794459b2424e9609f78dd3
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-07 22:23:52 +0530

    print 1 to 10

--- question.cpp
+++ question.cpp
@@ -208,4 +208,11 @@ int main() {
     b = temp;
 
 
-#Print 1 to 10
+//Print 1 to 10
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " ";
+    }
