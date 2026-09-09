4ee613fe406809f4061ef73a42db121c478468fd
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-09 22:05:34 +0530

    reverse number

--- question.cpp
+++ question.cpp
@@ -238,5 +238,9 @@ using namespace std;
 int main() {
     int n, rev = 0;
     cin >> n;
+    while (n > 0) {
+        int digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n / 10;
+    }
 
-    
