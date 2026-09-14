1f073b83baadd76366d1cd0beb726592ea16fbaf
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-14 22:12:32 +0530

    Relational operator

--- basic.cpp
+++ basic.cpp
@@ -106,3 +106,22 @@ int main(){
     return 0;
 
 }
+
+
+
+// Relational Operator
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 3;
+    int b = 5;
+    cout<< (a > b)<<endl;
+    cout<<(a < b)<<endl;
+    cout<<( a >= b )<<endl;
+    cout<< (a <= b)<< endl;
+    cout<<(a == b)<<endl;
+    cout<<(a != b)<<endl;
+
+
+    return 0;
+}
