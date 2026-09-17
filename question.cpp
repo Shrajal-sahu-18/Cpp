6531b14e7d74c5c56a18a13afb0c3638b7ac8eca
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-17 21:02:08 +0530

    Income tax calculator

--- question.cpp
+++ question.cpp
@@ -248,3 +248,26 @@
 
 //     return 0;
 // }
+
+//Income tax calculator
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int income ;
+//     float tax;
+//     cout << "Enter your income(In lakhs):";
+//     cin >> income;
+
+//     if (income  <= 5){
+//         tax = 0; 
+//     }else if(income <= 10){
+//         // 20%
+//         tax = 0.2 * income;
+//     }
+//     else {
+//         // 30%
+//         tax = 0.3 * income;    
+//     }
+//     cout << "Tax  = "<< (tax * 100000) << endl;
+//     return 0;
+// }
