1b416851a1a3d05eb4b704be7ab77e827524a7af
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-19 22:29:16 +0530

    number n to 1

--- basic.cpp
+++ basic.cpp
@@ -330,3 +330,19 @@
 //     }
 //     return 0;
 // }
+
+
+
+// n to 1
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout << "Enter number:";
+//     cin >> n ;
+//     for(int i = 1; n >= i; n--){
+//         cout << n << " ";
+
+//     }
+
+// }
