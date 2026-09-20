0dadc1bd35cd861184c13429eb8c5813f90553c9
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-20 22:02:31 +0530

    Number is Prime or Not

--- question.cpp
+++ question.cpp
@@ -399,3 +399,23 @@
 //     cout << res;
 //     return 0;
 // }
+
+
+
+// nmuber is prime or not
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 9;
+    bool isprime = true;
+    for(int i = 2; i <= n-1;i++){
+        if(n % i == 0){
+            isprime =false;
+            break;
+        }
+      }
+    if(isprime == true){
+        cout << "Number is prime"<< endl;
+    }
+    return 0;
+}
