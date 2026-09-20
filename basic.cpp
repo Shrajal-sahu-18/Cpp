6720cc70bc5042c7e9b5eed471a97d15bd6e6bdb
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-20 21:56:12 +0530

    Question break statement

--- basic.cpp
+++ basic.cpp
@@ -392,3 +392,22 @@
    
 //     return 0;
 // }
+
+
+
+// multiple of 10 break
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     do{
+//         cout << "Enter number:";
+//         cin >> n;
+//         if(n % 10 == 0){
+//             break;
+//         }
+//         cout << "You entered "<< n<< endl;
+
+//     }while(true);
+//      return 0;
+// }
