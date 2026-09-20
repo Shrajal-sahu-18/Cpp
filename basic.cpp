edc5d4e4b26a0635e3f4b2b655a862401ecc03be
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-20 21:53:40 +0530

    Do while loop

--- basic.cpp
+++ basic.cpp
@@ -346,3 +346,16 @@
 //     }
 
 // }
+
+// Do while loop
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i = 1;
+//     do {
+//         cout << i << " ";
+//         i++;
+//     }while(i <= 5);
+//     cout << endl;
+//     return 0;;
+// }
