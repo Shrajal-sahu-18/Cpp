e6bc138bd6641f3a257a75bfceb3e660932b296f
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-20 21:55:34 +0530

    break statement

--- basic.cpp
+++ basic.cpp
@@ -374,3 +374,21 @@
 //     }
 //     return 0;
 // }
+
+// Break 
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i = 1;
+//     while(i <= 10){
+//         if (i == 3){
+//             break;
+//         }
+//         cout << i << " ";
+//         i ++;
+        
+//     }
+   
+//     return 0;
+// }
