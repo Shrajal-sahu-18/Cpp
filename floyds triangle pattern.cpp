4ce11d065b3c90484d512343767e891acf1407f9
shrajal sahu <shrajalsahu169@gmail.com>
2026-09-22 22:41:55 +0530

    Update and rename floyds triangle pattern to floyds triangle pattern.cpp

--- floyds triangle pattern.cpp
+++ floyds triangle pattern.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     int num = 1;
-    //outer loop for rows
+    //outer loop for rows 
     for (int i = 1; i <= 5; i++) {
         //inner loop for columns
         for (int j = 1; j <= i; j++) {
