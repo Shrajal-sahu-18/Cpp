5022c8b8f0b496fc8be9b67ab1f18e4cd221b2e1
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-23 22:28:21 +0530

    Pattern

--- basic.cpp
+++ basic.cpp
@@ -441,5 +441,20 @@
 
 //     }while(true);
     
-    return 0;
+//     return 0;
+// }
+
+// Nested loop
+
+#include <iostream>
+using namespace std;
+int main(){
+    int i = 1;
+    for( i = 1; i <= 4; i++){
+        for(int j = 1; j <= 4 ; j++){
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    cout << i;
 }
