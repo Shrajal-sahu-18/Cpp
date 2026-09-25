18be37e1d48010963272071ddd3a1aeca148e3fb
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-25 23:26:56 +0530

    traingular pattern

--- basic.cpp
+++ basic.cpp
@@ -446,17 +446,29 @@
 
 // Nested loop
 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i = 1;
+//     for( i = 1; i <= 4; i++){
+//         for(int j = 1; j <= 4 ; j++){
+//             cout << i << " ";
+//         }
+//         cout << endl;
+//     }
+//     cout << i;
+// }
+
+//Pattern 
 #include <iostream>
 using namespace std;
 int main(){
-    int i = 1;
-    for( i = 1; i <= 4; i++){
-        for(int j = 1; j <= 4 ; j++){
-            cout << i << " ";
+    int n = 4;
+    for(int i = 1; i <= 4; i++){
+        for(int j = 1; j <= i ; j++){
+            cout << "*"<<" ";
         }
         cout << endl;
     }
-    cout << i;
+    return 0;
 }
-
-//Pattern 
