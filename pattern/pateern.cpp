d8de930a534498af05b77afae28e23dc8c8c7704
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-26 15:02:22 +0530

    create pattern cloumn

---
+++ pattern/pateern.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i = 1;
+//     for( i = 1; i <= 4; i++){
+//         for(int j = 1; j <= 4 ; j++){
+//             cout << i << " ";
+//         }
+//         cout << endl;
+//     }
+//     cout << i;
+// }
+
+
+// Traingular Pattern 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n = 4;
+//     for(int i = 1; i <= 4; i++){
+//         for(int j = 1; j <= i ; j++){
+//             cout << "*"<<" ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
